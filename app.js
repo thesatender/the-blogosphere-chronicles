31069db4d3433312f072fec8169d27f83dc23a22
Satender sharma <satender80010336@gmail.com>
2023-06-28 13:32:02 +0530

    Title modified

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const homeStartingContent = "Discover a world of engaging and insightful content on our blogging platform. We are a passionate community of writers, sharing knowledge and ideas on a wide range of topics. Whether you're seeking inspiration, seeking answers, or simply looking for an enjoyable read, we've got you covered. Begin your journey by exploring our featured articles on the home page. These handpicked gems represent the essence of our platform and will give you a taste of what's to come. We're thrilled to have you here and can't wait to embark on this exciting blogging adventure together. Join us on this captivating journey of knowledge, inspiration, and personal growth. Happy reading!";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 let posts = [];
-// let postTitle =""
-// let postContent =""
 
 
 app.set('view engine', 'ejs');
@@ -37,13 +35,20 @@ app.get("/compose", function (req, res) {
 
 app.get("/posts/:topic", function (req, res) {
   const reqTitle = _.lowerCase(req.params.topic);
-
+  let flag = 0;
+  let title =""
+  let content =""
   posts.forEach(function(post) {
     const storedTitle = _.lowerCase(post.title);
     if (storedTitle === reqTitle) {
-      res.render("post", {postTitle: post.title, postContent: post.content});
+      title = post.title;
+      content = post.content;
+      flag = 1;
     }
   });
+  if (flag ===1) {
+    res.render("post", {postTitle: title, postContent: content});
+  }
 
 });
 
